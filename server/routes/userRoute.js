13083ec8f732255997c8487dd4b3dea6b3b29d77
kar-thee <ourworldourpeople@gmail.com>
2021-11-19 23:38:15 +0530

    develop mode

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../shared/db_schema");
 
-const { encrypt, decryptPasswordFunc } = require("../util/hashFunc");
+const {
+  encryptPasswordFunc,
+  decryptPasswordFunc,
+} = require("../util/hashFunc");
 
 router.get("/", (req, res) => {
   res.send("reached user route");
@@ -20,7 +23,7 @@ router.post("/register", async (req, res) => {
       return res.status(400).send({ msg: "Email Already exists" });
     }
 
-    const hashedPassword = await encrypt(password);
+    const hashedPassword = await encryptPasswordFunc(password);
     const result = await User.create({ email, name, password: hashedPassword });
 
     res.send(result);
@@ -33,7 +36,27 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
-  res.json({ email, password });
+  try {
+    if (!email || !password) {
+      return res.status(400).send({ msg: "No empty values accepted" });
+    }
+
+    const userData = await User.findOne({ email });
+    if (!userData) {
+      return res.status(400).send({ msg: "Wrong email/password" });
+    }
+
+    const hashedPassword = userData.password;
+    const passwordMatch = await decryptPasswordFunc(password, hashedPassword);
+    if (!passwordMatch) {
+      return res.status(400).send({ msg: "Wrong email/password" });
+    }
+
+    res.json({ email, id: userData._id, msg: "HiFi" });
+  } catch (e) {
+    console.log(e, "err");
+    res.status(400).send({ msg: "Error in User login" });
+  }
 });
 
 module.exports = router;
--- server/util/hashFunc.js
+++ server/util/hashFunc.js
@@ -1,14 +1,14 @@
 const bcrypt = require("bcrypt");
 
-const encrypt = async (plainPass) => {
+const encryptPasswordFunc = async (plainPass) => {
   const salt = await bcrypt.genSalt(12);
   const encryptedData = await bcrypt.hash(plainPass, salt);
   return encryptedData;
 };
 
-const decrypt = async (plainPass, hashedPass) => {
+const decryptPasswordFunc = async (plainPass, hashedPass) => {
   const result = await bcrypt.compare(plainPass, hashedPass);
   return result;
 };
 
-module.exports = { encrypt, decrypt };
+module.exports = { encryptPasswordFunc, decryptPasswordFunc };
